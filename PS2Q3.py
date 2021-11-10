b66f8e47136bd0637f0085c8d18dcd0fa6c0717d
micahrscholes <94012887+micahrscholes@users.noreply.github.com>
2021-11-10 12:39:56 -0500

    Update PS2Q3.py

--- PS2Q3.py
+++ PS2Q3.py
@@ -9,6 +9,7 @@ Created on Wed Nov 10 11:51:47 2021
 import pandas as pd
 
 
+
 nhanes_2011 = pd.read_sas("/Users/micahscholes/Downloads/DEMO_G.XPT")
 nhanes_2011 = nhanes_2011.assign(cohort=2011)
 nhanes_2013 = pd.read_sas("/Users/micahscholes/Downloads/DEMO_H.XPT")
@@ -20,13 +21,14 @@ nhanes_2017 = nhanes_2017.assign(cohort=2017)
 nhanes_all = pd.concat([nhanes_2011, nhanes_2013, nhanes_2015, nhanes_2017])
 nhanes_reduced = nhanes_all[["SEQN", "RIDAGEYR", "RIDRETH3", "DMDEDUC2", 
                              "DMDMARTL", "RIDSTATR", "SDMVPSU", "SDMVSTRA", 
-                             "WTMEC2YR", "WTINT2YR", "cohort"]]
+                             "WTMEC2YR", "WTINT2YR", "RIAGENDR", "cohort"]]
 nhanes_reduced = nhanes_reduced.rename(
     columns={"SEQN":"id", "RIDAGEYR":"age", "RIDRETH3":"race_and_ethnicity", 
              "DMDEDUC2":"education", "DMDMARTL":"marital_status", 
              "RIDSTATR":"exam_status", "SDMVPSU":"masked_variance_pseudo-psu", 
              "SDMVSTRA":"masked_variance_pseudo-stratum", 
-             "WTMEC2YR":"mec_exam_weight", "WTINT2YR":"interview_weight"})
+             "WTMEC2YR":"mec_exam_weight", "WTINT2YR":"interview_weight", 
+             "RIAGENDR":"gender",})
 nhanes_reduced.age = nhanes_reduced.age.astype(int)
 nhanes_reduced.id = nhanes_reduced.id.astype(int)
 nas=["race_and_ethnicity", "education", "marital_status", "exam_status",
@@ -34,7 +36,7 @@ nas=["race_and_ethnicity", "education", "marital_status", "exam_status",
 nhanes_reduced[nas] = nhanes_reduced[nas].fillna(-1)
 nhanes_reduced[nas] = nhanes_reduced[nas].astype(int)
 nhanes_reduced.to_pickle("./nhanes.pkl")
-print(nhanes_reduced.shape[0])
+
 
 dental_2011 = pd.read_sas("/Users/micahscholes/Downloads/OHXDEN_G.XPT")
 dental_2011 = dental_2011.assign(cohort=2011)
@@ -68,7 +70,7 @@ dental_reduced = dental_all[["SEQN", "OHDDESTS"]+tooth_count+cavities_count
 for col in cavities_count:
     dental_reduced[col] = dental_reduced[col].str.decode('utf-8')
 dental_reduced = dental_reduced.rename(columns={"SEQN":"id",
-                                                "OHDDESTS":"dentician_status"})
+                                                "OHDDESTS":"ohx_status"})
 dental_reduced[tooth_count]=dental_reduced[tooth_count].fillna(-1)
 dental_reduced[tooth_count]=dental_reduced[tooth_count].astype(int)
 i = 1
@@ -86,8 +88,7 @@ for col in cavities_count:
         i = 18
 
 dental_reduced.id = dental_reduced.id.astype(int)
-dental_reduced.dentician_status = dental_reduced.dentician_status.astype(int)
+dental_reduced.ohx_status = dental_reduced.ohx_status.astype(int)
 
 
 dental_reduced.to_pickle("./dental.pkl")
-print(dental_reduced.shape[0])
